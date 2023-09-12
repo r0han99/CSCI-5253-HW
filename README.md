e6ad60b2dd60754760cf26d784a33ee2a70b7dec
r0han99 <r409067@gmail.com>
2023-09-12 17:14:48 -0600

    Homework-2: Pipeline & Docker - Created a dockerfile that creates a container, at entry point the script is executed with commandline argurments - added data-pipeline.py script which takes two commandline arguments at the moment 	- --read flag to extract data from the cloud 	- --transform (dob) option to transform the date of birth column and individualise then. Also save the transformed dataframe - updated readme with the course's intent.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# CSCI-5253-HW
-Data Center Scale Computing Couse
+# Data Center Scale Computing
+
+### CSCI-5253 - Fall 23 Dr. Alex Yarosh
+
+---
+
+### About 
+
+This repository holds all the future and current homework assignment files as pull requests individualised as different branches.
+
+---
+
---
+++ data-pipeline.py
@@ -0,0 +1,77 @@
+import pandas as pd 
+import argparse
+import time
+
+
+# reads data from the cloud 
+
+def extract_data():
+    return pd.read_csv('https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/people/people-1000.csv')
+
+
+def transformed(data):
+
+    # Transforms the data birth into 3 separate columns day month and year and drops userid
+    print("Transformation")
+    print('- Dropping redundancies')
+    data = data.drop('User Id',axis=1)
+        
+    # datetime conversion
+    print("- Converting to Datetime objects")
+    data['Date of birth'] = pd.to_datetime(data['Date of birth'])
+    
+
+    print("- Individualising Month, Day and Year to different columns")
+    data['month'] = data['Date of birth'].dt.month
+    data['year'] = data['Date of birth'].dt.year
+    data['day'] = data['Date of birth'].dt.day
+
+
+    # saving transformed dataset.
+    time.sleep(1)
+    print('- Transformation Done!')
+    print('- Saving Transformed data. transformed data is saved as /app/transformed-data.csv.')
+    data.to_csv('/app/transformed-data.csv',index=False)
+
+    # data.to_csv('transformed-data.csv',index=False)
+    # print("- Modified data is saved as transformed-data.csv")
+
+
+# prints head of data
+def show_data():
+    
+    data = extract_data()
+    print(data.head(10))
+
+def main(read_flag, transformation):
+    # calls all the required functions
+
+    if read_flag and transformation:
+        
+        data = extract_data()
+
+        transformed(data)
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Process a CSV file with transformations and save the results.")
+    parser.add_argument("--read", action="store_true", help="Flag to indicate reading data from the input CSV file")
+    parser.add_argument("--transform", help="Transformation option (example/custom)")
+    
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    if any(vars(args).values()):
+        
+        main(args.read, args.transform)
+        
+        print('-'*55)
+        print('Job Completed.')
+        print()
+
+    else:
+        print('No functionality detected. do python data-pipeline.py --help for manual.')
