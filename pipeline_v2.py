5ba8ba6961c378f59a92994550422466d1d55296
r0han99 <r409067@gmail.com>
2023-10-16 16:58:17 -0600

    Updated Code & Docker-Compose

--- pipeline_v2.py
+++ pipeline_v2.py
@@ -2,24 +2,34 @@
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
+import sys
 
 
 class etl:
 
-    def __init__(self, condition):
+    def __init__(self, condition, url=None):
+
+        # Condition = test, testing conditons 
+        # Condition = production, pulls the data from actual api endpoint
 
 
         print('Etl Pipeline')
         print('--'*25)
+        self.url = url
         self.condition = condition 
 
     def extract(self):
         
         if self.condition == 'test':
-            data = pd.read_json('https://data.austintexas.gov/resource/9t4d-g238.json')
+            data = pd.read_csv('./data/test/shelter1000.csv')
             
         else:
-            print('- API endpoint read configuration is not set. Complete the code.')
+            # https://data.austintexas.gov/resource/9t4d-g238.json
+            try:
+                data = pd.read_json(self.url)
+            except:
+                raise "Data Source Not Specified."
+            
 
         print("Data Received!")
         print('--'*25)
@@ -28,24 +38,18 @@ class etl:
 
     def transform(self, data):
 
-        
-        '''
-            - month year to separate columns, months to month names
-            - date time and date_of_birth to just date mm/dd/yy 
-            - drop outcome_subtype
-        '''
 
         print('Transforming Data.')
         
         labels = ['- Converting datetime and date_of_birth to datetime objects and transforming them to the pattern mm/dd/yyyy',
                   '- Filling NA.',
                   '- Creating ID for Outcome type and Outcome Event Type.',
-                  '- Correcting Column names.']
+                  '- Dividing into Entities']
         
         print(labels[0])
 
-        # data['datetime'] = pd.to_datetime(data['datetime']).dt.strftime('%d-%m-%Y')
-        # data['date_of_birth'] = pd.to_datetime(data['date_of_birth']).dt.strftime('%d-%m-%Y')
+        #data['datetime'] = pd.to_datetime(data['datetime']).dt.strftime('%d-%m-%Y')
+        #data['date_of_birth'] = pd.to_datetime(data['date_of_birth']).dt.strftime('%d-%m-%Y')
 
         print(labels[1])
 
@@ -53,19 +57,19 @@ class etl:
 
         print(labels[2])
 
-        data['Outcome_Type_ID'] = data.index + 1
-        data['Outcome_Event_ID'] = data.index + 1
+        data['outcome_type_id'] = data.index + 1
+        data['outcome_event_id'] = data.index + 1
 
         print(labels[3])
 
         # Dividing into entities 
         animal_table = ['animal_id', 'breed', 'color', 'name','date_of_birth','animal_type']
-        outcome_table = ['Outcome_Type_ID','outcome_type']
-        outcome_event = ['Outcome_Type_ID','datetime','sex_upon_outcome','outcome_subtype','animal_id',]
+        outcome_table = ['outcome_type_id','outcome_type']
+        outcome_event = ['outcome_event_id','datetime','sex_upon_outcome','outcome_subtype','animal_id',]
         data_colums_order = ['animal_id','breed','color',
                'name','date_of_birth','animal_type','datetime',
                'sex_upon_outcome','outcome_subtype',
-               'Outcome_Type_ID','Outcome_Event_ID','outcome_type']
+               'outcome_type_id','outcome_event_id','outcome_type']
 
         # re-ordering
         data = data[data_colums_order]
@@ -73,72 +77,11 @@ class etl:
         outcomes = data[outcome_table]
         outcome_events = data[outcome_event]
 
-        # correcting column names
-
-        animal_sql_cols =[
-            'Animal_ID',
-            'Breed',
-            'Color',
-            'Name',
-            'Date_of_Birth',
-            'Animal_Type'
-        ]
-
-        outcome_sql_cols = [
-            'Outcome_Type_ID',
-            'Outcome_Type'
-        ]
-
-        outcomeevents_sql_cols = [
-            'Outcome_Event_ID',
-            'DateTime',
-            'Sex_upon_Outcome',
-            'Outcome_Subtype',
-            'Animal_ID',
-        ]
-
-        fact_table_columns = [
-            'Animal_ID',
-            'Breed',
-            'Color',
-            'Name',
-            'Date_of_Birth',
-            'Animal_Type',
-            'DateTime',
-            'Sex_upon_Outcome',
-            'Outcome_Subtype',
-            'Outcome_Type_ID',
-            'Outcome_Event_ID',
-            'Outcome_Type'
-        ]
-
-        animal_sql_cols = [column.lower() for column in animal_sql_cols]
-        outcome_sql_cols = [column.lower() for column in outcome_sql_cols]
-        outcomeevents_sql_cols = [column.lower() for column in outcomeevents_sql_cols]
-        fact_table_columns = [column.lower() for column in fact_table_columns]
-
-
-
-        animal.columns = animal_sql_cols
-        outcomes.columns = outcome_sql_cols
-        outcome_events.columns = outcomeevents_sql_cols
-        data.columns = fact_table_columns
-
-
         # Correcting Duplication
         animal.drop_duplicates(inplace=True)
-        outcomes.drop_duplicates(inplace=True)
-        outcome_events.drop_duplicates(inplace=True)
-        data.drop_duplicates(inplace=True)
-
-
         outcomes = pd.DataFrame(pd.Series(outcomes['outcome_type'].unique(),name='outcome_type'))
         outcomes['outcome_type_id'] = outcomes.index + 1 
         outcomes = outcomes[['outcome_type_id','outcome_type']]
-        
-
-
-
         
 
 
@@ -186,8 +129,12 @@ class etl:
 
 if __name__ == '__main__':
 
+    # Data Source 
+    args = sys.argv
+    data_url = args[1]
+
     # etl_obj
-    etl_obj = etl(condition='test')
+    etl_obj = etl(condition="production", url=data_url)
 
     data = etl_obj.extract()
     transformed_data = etl_obj.transform(data) # returns transformed 
